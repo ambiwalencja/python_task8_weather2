f8c36e6a032af1630987373b784161cf43e0faea
ambiwalencja <helena.a.sokolowska@gmail.com>
2021-07-03 13:54:32 +0200

    cosmetic changes in class OneDayWeather and removal of comments

--- weather_wf.py
+++ weather_wf.py
@@ -1,13 +1,12 @@
 import sys
 import requests  # https://docs.python-requests.org/en/master/
 import datetime
-from pprint import pprint
 
 
 class OneDayWeather:
-    def __init__(self):
-        self.rain = 0
-        self.snow = 0
+    def __init__(self, rain, snow):
+        self.rain = rain
+        self.snow = snow
 
     def output_message(self):  # this method returns message
         if self.rain == 1 and self.snow == 1:
@@ -32,16 +31,12 @@ class WeatherForecast:
         self.counter = 0
 
     def set_date(self):
-        # if sys.argv[2] == None
         if len(sys.argv) < 3:
-            # print(f'set date: {datetime.datetime.today().date() + datetime.timedelta(days=1)}')
             return datetime.datetime.today().date() + datetime.timedelta(days=1)
-        # print(f'set date 2: {datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()}')
         return datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
 
     def check_date(self):  # true if correct date
         difference = self.date - datetime.datetime.today().date()
-        # print(f'check date: difference {difference.days}')
         return difference.days <= 10 and difference.days >= 0
 
     def read_forecast_from_file(self):  # reads from file and adds result to dictionary
@@ -49,10 +44,9 @@ class WeatherForecast:
             for line in file.readlines():
                 current_line = line.split(',')
                 current_date = datetime.datetime.strptime(current_line[0], "%Y-%m-%d").date()
-                self.forecast_dict[current_date] = OneDayWeather()
-                self.forecast_dict[current_date].rain = int(current_line[1])
-                self.forecast_dict[current_date].snow = int(current_line[2].split("\n")[0])
-            # print(f'read from file: current line {current_line}')
+                current_rain = int(current_line[1])
+                current_snow = int(current_line[2].split("\n")[0])
+                self.forecast_dict[current_date] = OneDayWeather(current_rain, current_snow)
 
     def get_data(self):
         request_params = {'key': self.key, 'q': self.town, 'days': self.forecast_length, 'dt': self.date}
@@ -60,7 +54,6 @@ class WeatherForecast:
         self.current_weather.rain = response.json()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
         self.current_weather.snow = response.json()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
         print(f'get data: current rain {self.current_weather.rain}')
-        # pprint(response.json())
         # obsługa błędu!!! zawsze jak działam z API to muszę to obsłużyć,
         # bo to jest korzystanie z zewnętrznej usługi i nie mam wpływu na to, czy ktoś tego nie zmieni
         # sprawdzić, czy struktura się zgadza, poszczególne klucze istnieją itp
@@ -74,14 +67,11 @@ class WeatherForecast:
 
     def get_forecast_for_date(self):
         self.read_forecast_from_file()
-        # print(f'get forecast for date 0: dict {self.forecast_dict}, and current date: {self.date}')
         if self.date in self.forecast_dict.keys():  # if date is already in file, take it
-            # print(f'get forecast for date 1: read from dict {self.forecast_dict[self.date].rain}')
             self.current_weather.rain = self.forecast_dict[self.date].rain
             self.current_weather.snow = self.forecast_dict[self.date].snow
 
         else:
-            # print(f'get forecast for date 2: get from API {self.current_weather.rain}')
             self.get_data()  # if not, get data from API
             self.write_to_file(self.current_weather)  # and add recent forecast to file
         return self.current_weather.output_message()
